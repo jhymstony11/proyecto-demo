04f6df7acbd18e32a1a7e0471af37eec6fa3cc13
jhymstony11 <118759772+jhymstony11@users.noreply.github.com>
2022-11-21 14:18:19 -0500

    Update tarea13.js

--- js/tarea13.js
+++ js/tarea13.js
@@ -1,49 +1,49 @@
 // EJERCIO  1
-//  let num1 = parseInt(prompt("Ingrese el primer numero:"));
-//  let num2 = parseInt(prompt("Ingrese el segundo numero:"));
-//  let resultado = num1 + num2;
-//  let resultado2 = num1 * num2;
-//  alert(`La suma es de ${resultado} y la multipicacion es de ${resultado2}`);
+ let num1 = parseInt(prompt("Ingrese el primer numero:"));
+  let num2 = parseInt(prompt("Ingrese el segundo numero:"));
+  let resultado = num1 + num2;
+  let resultado2 = num1 * num2;
+ alert(`La suma es de ${resultado} y la multipicacion es de ${resultado2}`);
 
 
 
 //EJERCIO  2
-// let num3 = parseInt(prompt("Ingrese un numero:"));
-// let num4 = num3 * 0.2;
-// alert(`El 20% del numero es ${num4}`);
+ let num3 = parseInt(prompt("Ingrese un numero:"));
+ let num4 = num3 * 0.2;
+ alert(`El 20% del numero es ${num4}`);
 
 
 //EJERCICIO 3
 
-// let numero5 = 0;
-// numero5 = prompt("Ingrese numero en metros: ");
-// let convertircm = parseInt(numero5) * 100;
-// let convertirmm = parseInt(numero5) * 1000;
-// alert(`Equivale a ${convertircm} cm y ${convertircm} mm`);
+ let numero5 = 0;
+ numero5 = prompt("Ingrese numero en metros: ");
+ let convertircm = parseInt(numero5) * 100;
+ let convertirmm = parseInt(numero5) * 1000;
+ alert(`Equivale a ${convertircm} cm y ${convertircm} mm`);
 
 
 // EJERCICIO 4
 
-// let base = 0;
-// let altura = 0;
-// base=prompt("ingrese el valor de la base:");
-// altura=prompt("ingrese el valor de la altura:");
-// let area = parseInt(base*altura/2);
-// alert(`El area del triangulo es: ${area} unidades`); 
+ let base = 0;
+ let altura = 0;
+ base=prompt("ingrese el valor de la base:");
+ altura=prompt("ingrese el valor de la altura:");
+ let area = parseInt(base*altura/2);
+ alert(`El area del triangulo es: ${area} unidades`); 
 
 // EJERCICIO 5
-// let dolares = 0;
-// dolares = prompt("Ingrese monto en dolares: ");
-// let convertir = parseInt(dolares) * 3.38;
-// alert(`Equivale a ${convertir} soles`);
+ let dolares = 0;
+ dolares = prompt("Ingrese monto en dolares: ");
+ let convertir = parseInt(dolares) * 3.38;
+ alert(`Equivale a ${convertir} soles`);
 
 // EJERCICIO 6
 
 
 
 
-// var x;
-//  x=prompt('Ingrese el valor de x: ');
-//  y=prompt('Ingrese el valor de y: ');
-//  suma=parseInt(x)+parseInt(y);
-//  document.write(`La suma es: ${suma}`);
+ var x;
+ x=prompt('Ingrese el valor de x: ');
+  y=prompt('Ingrese el valor de y: ');
+  suma=parseInt(x)+parseInt(y);
+  document.write(`La suma es: ${suma}`);
